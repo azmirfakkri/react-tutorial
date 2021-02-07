62258c24bafd8650d568d2600a9ca6bfa6c4ee55
azmirfakkri <azmirfakkri@gmail.com>
2021-02-07 16:23:28 +0000

    update props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// css - full path
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const books = [{
+  id: 1,
+  author: 'Steve Mann',
+  title: 'Easy Peasy Puppy Squeezy: Your simple step-by-step guide to raising and training a happy puppy or dog',
+  img: 'https://images-eu.ssl-images-amazon.com/images/I/81X9FfdcX%2BL._AC_UL200_SR200,200_.jpg'
+},
+{
+  id: 2,
+  author: 'Anna Dewdney',
+  title: 'Llama Llama I Love You',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/918sHoEOD8L._AC_UL200_SR200,200_.jpg'
+},
+{
+  id: 3,
+  author: 'George Orwell',
+  title: '1984 (Signet Classics)',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/71kxa1-0mfL._AC_UL200_SR200,200_.jpg'
+}];
+
+
+function BookList() {
+  return (
+    <section className='booklist'> 
+      {books.map((bookObject) => {
+        return <Book key={bookObject.id} {...bookObject}></Book>;
+      })}
+    </section>
+  );
+}
+
+const Book = (props) => {
+  const {img, title, author} = props;
+  return (
+    <article className='book'>
+      <img src={img} alt=""/>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+
+ReactDom.render(<BookList></BookList>, document.getElementById('root'));
